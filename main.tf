faa9a8f1b6d1383ded615626c9742881ca186028
Ubuntu <ubuntu@ip-172-31-23-85.ap-south-1.compute.internal>
2026-04-15 11:22:34 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "instance-1" {
+  ami           = "ami-07216ac99dc46a187"
+  instance_type = "t3a.small"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
