339e762891830ca544defa2eab5b09a1122a3dc6
ankit-cloud9 <ankitchandanpat.niveda@gmail.com>
2026-04-16 12:26:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "instance-1" {
   ami           = "ami-07216ac99dc46a187"
   instance_type = "t3a.small"
   tags = {
-      Name = "TF-Instance"
+      Name = "TF-Instance",
+      Env = "dev"
   }
 }
